b555c7a9b99690144f1c31aac6def4e3a1ee34a0
Stefano Casasola <ste.casasola@gmail.com>
2021-02-01 12:08:23 +0100

    Finishes the exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { connect } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap'
 
-function App() {
+// mapStateToProps is a function that returns an object
+// this object will contain the redux store properties you want
+// your component to receive as props
+const mapStateToProps = (state) => ({ count: state.count, mickey: state.mickey })
+
+// mapDispatchToProps is a function that returns an object
+// this object will contain the functions that we'll be able
+// to dispatch from our component
+const mapDispatchToProps = (dispatch) => ({
+  increment: () =>
+    dispatch({
+      type: 'INCREMENT', // this is the action we're dispatching
+    }),
+
+  decrement: () => dispatch({ type: 'DECREMENT' }), // this is the action we're dispatching
+
+  changeName: () =>
+    dispatch({
+      type: 'CHANGE_NAME', // this is the action we're dispatching
+      payload: 'Stefano',
+    }),
+})
+
+function App({ count, increment, decrement, changeName, mickey }) {
+  console.log(mickey)
+  console.log(changeName)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ fontSize: '5rem' }}>
+        <Button variant="info" onClick={increment}>
+          +
+        </Button>
+        {count}
+        <Button variant="info" onClick={decrement}>
+          -
+        </Button>
+        <div onClick={changeName}>{mickey}</div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+// connect needs two parameters, which are
+// the two functions mapStateToProps and mapDispatchToProps
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,6 +10,20 @@ export const mainReducer = (state = initialState, action) => {
         count: state.count + 1,
       }
 
+    case 'DECREMENT':
+      return {
+        ...state,
+        // mickey: state.mickey,
+        // strive: state.strive,
+        count: state.count - 1,
+      }
+
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        mickey: action.payload,
+      }
+
     default:
       return state
   }
